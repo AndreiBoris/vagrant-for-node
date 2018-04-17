2ea20f7cd16adc5c17918c8d5fbd1deaf5389ae5
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2018-04-17 15:31:00 -0400

    Enable mod_rewrite for apache2

--- provision-scripts/apache.sh
+++ provision-scripts/apache.sh
@@ -2,3 +2,5 @@ echo "=================== Running apache provisioning script (apache.sh)"
 
 echo "=================== Install Apache2..."
 sudo apt-get install -y apache2
+sudo a2enmod rewrite
+sudo service apache2 restart
